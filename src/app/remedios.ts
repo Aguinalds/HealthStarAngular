1ff72089720b6da69d8c4ba1f38ebdd821ac6955
Aguinaldo <guimachado1708@gmail.com>
2021-10-29 19:28:10 -0300

    Lista de Remedios

---
+++ src/app/remedios.ts
@@ -0,0 +1,21 @@
+export const Remedios = [
+
+    {
+        id: 1,
+        nome: 'Remedio 1',
+        desc: 'Descrição',
+        foto: ''
+    },
+    {
+        id: 2,
+        nome: 'Remedio 2',
+        desc: 'Descrição 2',
+        foto: ''
+    },
+    {
+        id: 3,
+        nome: 'Remedio 3',
+        desc: 'Descrição 3',
+        foto: ''
+    },
+]
