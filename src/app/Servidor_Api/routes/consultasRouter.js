b173f40f6d87d9d4140e31720fdd950cc89987f6
Lincoln-Andrade-Silva <lincolnandradesilva@outlook.com>
2021-11-22 18:14:15 -0300

    routes

---
+++ src/app/Servidor_Api/routes/consultasRouter.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+/* GET lista de consultass */
+router.get('/', consultasController.listar);
+
+/* POST salvar de consultass */
+router.post('/', consultasController.salvar);
+
+/* GET buscar de consultass */
+router.get('/:consultasId', consultasController.buscarPorId);
+
+/* PUT atualizar de consultass */
+router.put('/:consultasId', consultasController.atualizar);
+
+/* DELETE o consultas */
+router.delete('/:consultasId', consultasController.excluir);
+
+
+module.exports = router;
