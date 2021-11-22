2adf72c7a25c2944135d2c2105b0a1a90837b64c
Lincoln-Andrade-Silva <lincolnandradesilva@outlook.com>
2021-11-22 18:02:47 -0300

    consulta services

--- src/app/services/consulta.service.ts
+++ src/app/services/consulta.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Consulta } from '../models/consulta.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConsultaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  url = "http://localhost:3000/consultas";
+
+  listar(): Observable<Consulta[]> {
+    return this.http.get<Consulta[]>(this.url);
+  }
+  
+  salvar(consultas: Consulta): Observable<Consulta> {
+    return this.http.post<Consulta>(this.url, consultas);
+  }
+
+  buscarPorId(id: number): Observable<Consulta> {
+    const path = `${this.url}/${id}`;
+    return this.http.get<Consulta>(path);
+  }
+
+  atualizar(consulta: Consulta): Observable<Consulta> {
+    const path = `${this.url}/${consulta.id}`;
+    return this.http.put<Consulta>(path, consulta);
+  }
+
+  excluir(id? : number): Observable<Consulta> {
+    const path = `${this.url}/${id}`;
+    return this.http.delete<Consulta>(path);
+  }
 }
